26f6423acf4dffcdf36698c6e8ddcfb10cdb1245
Mohamed Ali BARKA <mohamed-ali.barka@illuin.tech>
2025-08-13 13:36:45 +0200

    Changed student homepage layout
    GitOrigin-RevId: 860c3af0372795449b9ca5f9aff43f5d065cea31

--- front/src/app/modules/shell/interfaces/session.ts
+++ front/src/app/modules/shell/interfaces/session.ts
@@ -2,6 +2,7 @@ import { IBasicCourse } from "./course";
 
 export interface ISession {
   id: number;
+  title: string;
   startDateTime: number;
   endDateTime: number;
   blockContainerCreationBeforeStartTime: boolean;
