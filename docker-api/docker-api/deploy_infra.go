b11c7e0cfde2be8c0c9de7cad89f11a3d52b9da0
Mohamed-Ali Barka <mohamed-ali.barka@illuin.tech>
2025-12-02 10:52:21 +0000

    Merge branch 'add-shared-pool-if-no-course' into 'master'
    Add shared-pool label if a worker has no course ids
    
    See merge request mydocker/mydockergoapp!85
    
    GitOrigin-RevId: 553dc2a2501fb0e34d6791f228a92dc9a546eda6

--- docker-api/docker-api/deploy_infra.go
+++ docker-api/docker-api/deploy_infra.go
@@ -30,10 +30,14 @@ func (s *server) DeployInfra(request *pb.DeployRequest, responseStream pb.Contai
 		ownerWorkers, exist := flavor[w.Owner]
 		labels := []string{}
 
-		log.Infof("Adding course id labels %v", w.GetCourseIds())
-		for _, courseId := range w.GetCourseIds() {
-			log.Info("Adding course id label ", courseId)
-			labels = append(labels, fmt.Sprintf("courseId-%s", courseId))
+		if len(w.GetCourseIds()) == 0 {
+			log.Infof("Adding shared-pool label - region : %s, flavor : %s, owner: %s", w.GetRegion(), w.GetFlavor(), w.GetOwner())
+			labels = append(labels, "shared-pool")
+		} else {
+			for _, courseId := range w.GetCourseIds() {
+				log.Infof("Adding course id label for course %s - region : %s, flavor : %s, owner: %s", courseId, w.GetRegion(), w.GetFlavor(), w.GetOwner())
+				labels = append(labels, fmt.Sprintf("courseId-%s", courseId))
+			}
 		}
 
 		if exist {
